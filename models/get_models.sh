0f343b1c7fb1bfab96af2069b6516357fa91726a
Weronika <wgutfete@stud.elka.pw.edu.pl>
2021-09-27 15:31:21 +0200

    models' dir

---
+++ models/get_models.sh
@@ -0,0 +1,2 @@
+wget https://drive.google.com/file/d/12lXTHx73SewOEWyjvnVvFO5UO0RbVfT0/view?usp=sharing
+wget depth https://drive.google.com/file/d/1mwt4RRv4qaFnj2TwtQKvavdu0bFSNLNO/view?usp=sharing
